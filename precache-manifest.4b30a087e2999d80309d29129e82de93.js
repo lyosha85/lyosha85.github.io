ddd4c3c1438a527df5fdc06255cda528ca16d397
Alex <lyosha85@gmail.com>
2019-07-30 19:37:06 -0700

    Updates

---
+++ precache-manifest.4b30a087e2999d80309d29129e82de93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "773f3d4506dc58615ac8eff88292721e",
+    "url": "/lyosha85.github.io/index.html"
+  },
+  {
+    "revision": "e421516992356a4c611d",
+    "url": "/lyosha85.github.io/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/lyosha85.github.io/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "e421516992356a4c611d",
+    "url": "/lyosha85.github.io/static/js/main.6c7f26e4.chunk.js"
+  },
+  {
+    "revision": "8a8a1e1fcf93e2faa517",
+    "url": "/lyosha85.github.io/static/js/runtime~main.77b370a7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lyosha85.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
